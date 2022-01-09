0fa39e71d9f0cfb3090f31e353f051047e37e209
Niro Lin <425060468@qq.com>
2022-01-08 20:11:55 -0500

    feat: looks like a social media

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,71 +1,91 @@
+import React from 'react';
 import logo from './logo.png';
 import './App.css';
+// import API from '../../my-worker/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Post Everyday as Anyone</p>
-        </div>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      textContent: 'Something is happening...',
+    };
 
-        <div className="Message">
-          <box className="box">
-                    
-            <p>Edit and Post your life</p>
-            
-            <textarea className="user-name">
-              What's the name you want to use today?
-            </textarea>
+    this.PostRequest = this.PostRequest.bind(this);
+    this.GetRequest = this.GetRequest.bind(this);
+  }
 
-            <p></p>
+  PostRequest(){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('POST', 'http://127.0.0.1:8787', true);
+    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-            <textarea className="textarea">
-              Say something...
-            </textarea>
+    var user_name = document.getElementById('user_name').value;
+    var user_post = document.getElementById('user_post').value;
+    var data = "user_name=" + user_name + 
+      "&&&&& user_post=" + user_post;
 
-            <p></p>
+    httpRequest.send(data);
 
-            <button
-              className="post-btn"
-            >
-              POST
-            </button>
+    var new_text = user_name + ": " + user_post + "\n\n" + this.state.textContent;
+    this.setState({
+      textContent: new_text,
+    });
+  }
+  
+  GetRequest(){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'http://127.0.0.1:8787', true);
+    httpRequest.send();
+  
+    let json = '';
+    httpRequest.onreadystatechange = () => {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+          json = httpRequest.responseText;
+          this.setState({
+            textContent: json,
+          });
+          console.log(this.state.textContent);
+      }
+    }
+  }
 
-          </box>
-
-          <box className="box">
-            <p>See how is everyone going</p>
-            <textarea className="all-post" readonly="readonly">
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
-                TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Post Everyday as Anyone</p>
+          </div>
+          <div className="Message">
+            <box className="box">
+              <p>Post your life</p>
+              <textarea id='user_name' className="user-name" defaultValue="What's the name you want to use today?">
               </textarea>
+              <p></p>
+              <textarea id='user_post' className="textarea" defaultValue='Say something...'>
+              </textarea>
+              <p></p>
+              <button className="post-btn" onClick={this.PostRequest}>
+                POST
+              </button>
+            </box>
             
-            <p></p>
-
-            <button
-              className="post-btn"
-            >
-              REFRESH
-            </button>
-
-          </box>
-        </div>
-      </header>
-
-    </div>
-  );
+            <box className="box">
+              <p>See how is everyone going</p>
+              <p></p>
+              <textarea className="all-post" value={this.state.textContent} readOnly="readOnly">
+              </textarea>
+              <p></p>
+              <button className="post-btn" onClick={this.GetRequest}>
+                REFRESH
+              </button>
+            </box>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
